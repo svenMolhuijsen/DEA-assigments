6e7fb28274fbfe3b018ddff6f8f29863282ccb62
Sven Molhuijsen <s.molhuijsen@gmail.com>
2019-02-19 16:35:44 +0100

    created singleton example

---
+++ Week 3/singleton/src/main/java/nl/han/ica/oose/dea/Singleton.java
@@ -0,0 +1,22 @@
+package nl.han.ica.oose.dea;
+
+    public class Singleton {
+
+        private static Singleton singleton = new Singleton( );
+
+        /* A private Constructor prevents any other
+         * class from instantiating.
+         */
+        private Singleton() { }
+
+        /* Static 'instance' method */
+        public static Singleton getInstance( ) {
+            return singleton;
+        }
+
+        /* Other methods protected by singleton-ness */
+        protected void demoMethod( ) {
+            System.out.println("demoMethod for singleton");
+        }
+    }
+
